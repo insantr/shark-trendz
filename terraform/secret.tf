6b7d6bf91158cee02a69addb1c1df7ad8cfa9a4d
Alexander Komarica <aleksandr.komarica@ringcentral.com>
2024-03-30 16:43:37 +0100

    test19

--- terraform/secret.tf
+++ terraform/secret.tf
@@ -11,8 +11,8 @@ resource "google_secret_manager_secret_version" "service-account-key-version" {
   secret_data = file(var.service_account_key_file)
 }
 
-resource "google_service_account" "cloud_run_account" {
-  account_id   = "cloud-run-service-account"
+resource "google_service_account" "default" {
+  account_id   = "cloud-run-sa"
   display_name = "Service account for Cloud Run"
 }
 
@@ -20,6 +20,6 @@ resource "google_secret_manager_secret_iam_member" "default" {
   secret_id = google_secret_manager_secret.service-account-key.id
   role      = "roles/secretmanager.secretAccessor"
   # Grant the new deployed service account access to this secret.
-  member     = "serviceAccount:${google_service_account.cloud_run_account.email}"
+  member     = "serviceAccount:${google_service_account.default.email}"
   depends_on = [google_secret_manager_secret.service-account-key]
 }

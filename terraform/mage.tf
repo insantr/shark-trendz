94d954b89f9ec682d799b4cba244f9c4a31c037e
Alexander Komarica <aleksandr.komarica@ringcentral.com>
2024-03-30 16:25:17 +0100

    test16

--- terraform/mage.tf
+++ terraform/mage.tf
@@ -135,6 +135,7 @@ resource "google_cloud_run_service" "run_service" {
           }
         }
       }
+      service_account = google_service_account.default.email
     }
 
     metadata {
--- terraform/secret.tf
+++ terraform/secret.tf
@@ -10,3 +10,16 @@ resource "google_secret_manager_secret_version" "service-account-key-version" {
   secret      = google_secret_manager_secret.service-account-key.name
   secret_data = file(var.service_account_key_file)
 }
+
+resource "google_service_account" "default" {
+  account_id   = "cloud-run-service-account"
+  display_name = "Service account for Cloud Run"
+}
+
+resource "google_secret_manager_secret_iam_member" "default" {
+  secret_id = google_secret_manager_secret.service-account-key.id
+  role      = "roles/secretmanager.secretAccessor"
+  # Grant the new deployed service account access to this secret.
+  member     = "serviceAccount:${google_service_account.default.email}"
+  depends_on = [google_secret_manager_secret.service-account-key]
+}
